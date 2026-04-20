a68416514395d53484fa67f98373ee672920b1d0
kanj-locus <kanj@locus.sh>
2026-04-20 10:25:24 +0530

    Add spacings

---
+++ EncoreSwiftUiKit/EncoreSwiftUiKit/Common/Spacing.swift
@@ -0,0 +1,22 @@
+import CoreGraphics
+
+enum Spacing {
+    static let spacing2:  CGFloat = 2
+    static let spacing4:  CGFloat = 4
+    static let spacing6:  CGFloat = 6
+    static let spacing8:  CGFloat = 8
+    static let spacing12: CGFloat = 12
+    static let spacing16: CGFloat = 16
+    static let spacing20: CGFloat = 20
+    static let spacing24: CGFloat = 24
+    static let spacing32: CGFloat = 32
+    static let spacing40: CGFloat = 40
+    static let spacing48: CGFloat = 48
+    static let spacing56: CGFloat = 56
+    static let spacing60: CGFloat = 60
+    static let spacing64: CGFloat = 64
+    static let spacing72: CGFloat = 72
+    static let spacing80: CGFloat = 80
+    static let spacing88: CGFloat = 88
+    static let spacing96: CGFloat = 96
+}
