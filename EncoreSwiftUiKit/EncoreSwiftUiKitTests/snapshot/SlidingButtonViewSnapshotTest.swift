c0bd05fc8b6075cf6345ef6f0cc1821f6e83e179
kanj-locus <kanj@locus.sh>
2026-05-06 13:46:50 +0530

    Delete snapshot test

--- EncoreSwiftUiKit/EncoreSwiftUiKitTests/snapshot/SlidingButtonViewSnapshotTest.swift
+++
@@ -1,7 +0,0 @@
-import SnapshottingTests
-
-class SlidingButtonViewSnapshotTest: SnapshotTest {
-    override class func snapshotPreviews() -> [String]? {
-        return ["SlidingButton-Enabled"]
-    }
-}
