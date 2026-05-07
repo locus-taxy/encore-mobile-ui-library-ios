865ef64f4b1796bf2fcb7e98dd3eb59d7be376b9
kanj-locus <kanj@locus.sh>
2026-05-07 13:31:54 +0530

    Reviewed

--- EncoreSwiftUiKit/EncoreSwiftUiKitTests/snapshot/CircularProgressViewSnapshotTest.swift
+++
@@ -1,7 +0,0 @@
-import SnapshottingTests
-
-class CircularProgressViewSnapshotTest: SnapshotTest {
-    override class func snapshotPreviews() -> [String]? {
-        return ["CircularProgress-Determinate"]
-    }
-}
--- EncoreSwiftUiKit/EncoreSwiftUiKitTests/snapshot/LinearProgressViewSnapshotTest.swift
+++
@@ -1,7 +0,0 @@
-import SnapshottingTests
-
-class LinearProgressViewSnapshotTest: SnapshotTest {
-    override class func snapshotPreviews() -> [String]? {
-        return ["LinearProgress-Determinate"]
-    }
-}
